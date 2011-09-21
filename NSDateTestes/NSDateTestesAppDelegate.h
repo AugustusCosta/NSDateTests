4d82322ca1482638f5123415a197e1b5cff1bc3a
Augustus Costa <augustuscosta@Augustus-Costas-Mac-mini.local>
2011-09-21 10:07:05 -0300

    Initial Commit

---
+++ NSDateTestes/NSDateTestesAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  NSDateTestesAppDelegate.h
+//  NSDateTestes
+//
+//  Created by Augustus Costa on 9/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class NSDateTestesViewController;
+
+@interface NSDateTestesAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet NSDateTestesViewController *viewController;
+
+@end
---
+++ NSDateTestes/NSDateTestesViewController.h
@@ -0,0 +1,15 @@
+//
+//  NSDateTestesViewController.h
+//  NSDateTestes
+//
+//  Created by Augustus Costa on 9/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSDateTestesViewController : UIViewController {
+    
+}
+
+@end
